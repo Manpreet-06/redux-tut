6457cb3556535f81aef0ebb06ce63676f11ea3ce
Manpreet Arora <82157451+Manpreet-Arora@users.noreply.github.com>
2023-10-06 17:35:12 +0530

    Add: Add to cart and remove from cart reucer and actions added

---
+++ src/Services/Reducers/reducer.js
@@ -0,0 +1,20 @@
+import { ADD_TO_CART, REMOVE_TO_CART } from "../Constants";
+
+const initialState = {
+  cartData: [],
+};
+
+export default function cartItems(state = [], action) {
+  switch (action.type) {
+    case ADD_TO_CART:
+        console.log("" ,action);
+      return [...state, { cartData: action.data }];
+      break;
+    case REMOVE_TO_CART:
+        state.pop()
+      return [...state];
+      break;
+    default:
+      return state;
+  }
+}
